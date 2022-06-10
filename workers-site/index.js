c994c1340eecf5b6abed0ed9c03e2e671a0d71c9
Joaquin Florius <j@florius.com.ar>
2022-06-10 13:28:32 +0200

    some cleanup

--- workers-site/index.js
+++ workers-site/index.js
@@ -1,42 +1,37 @@
 import { getAssetFromKV } from '@cloudflare/kv-asset-handler'
 
-const DEBUG = true
 
 addEventListener('fetch', event => {
   event.respondWith(handleEvent(event))
 })
 
 async function handleEvent(event) {
-  let options = {}
-
   try {
-    if (DEBUG) {
-      options.cacheControl = {
-        bypassCache: true,
-      }
-    }
-
-    return await getAssetFromKV(event, options)
-  } catch (e) {
-    let pathname = new URL(event.request.url).pathname.substring(1)
-    let count;
-    if (pathname === "") {
-      count = Math.floor(Math.random() * 1000);
-    } else {
-      count = parseInt(pathname)
-      if (isNaN(count)) {
-        count = pathname.length
-      }
-    }
     try {
-      let replaced = await getAssetFromKV(event, {
-        mapRequestToAsset: req => new Request(`${new URL(req.url).origin}/template.html`, req),
-      })
-
-      return new HTMLRewriter().on('*', new ElementHandler(count)).transform(replaced);
+      return await getAssetFromKV(event)
     } catch (e) {
-      return new Response(e.message || e.toString(), { status: 500 })
+      if (e instanceof NotFoundError) {
+        let pathname = new URL(event.request.url).pathname.substring(1)
+        let count;
+        if (pathname === "") {
+          count = Math.floor(Math.random() * 1000);
+        } else {
+          count = parseInt(pathname)
+          if (isNaN(count)) {
+            count = pathname.length
+          }
+        }
+        let replaced = await getAssetFromKV(event, {
+          mapRequestToAsset: req => new Request(`${new URL(req.url).origin}/template.html`, req),
+        })
+
+        return new HTMLRewriter().on('*', new ElementHandler(count)).transform(replaced);
+      } else {
+        throw e
+      }
     }
+  } catch (e) {
+    return new Response(e.message || e.toString(), { status: 500 })
   }
 }
 
